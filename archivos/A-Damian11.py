72183d27acabcd358f5db95513799f19325d97c6
A-Damian11 <alandamian11.fr@gmail.com>
2020-07-17 10:34:17 -0500

    ya hice las propiedades de cada cosa

---
+++ archivos/A-Damian11.py
@@ -0,0 +1,19 @@
+class mouse:
+    def _init_(self,marca,tipo,color)
+    self._marca = marca
+    self._tipo = tipo
+    sel._color = color
+
+@property
+def marca(self):
+    return self._marca
+
+@property
+def tipo(self):
+    return self._tipo
+
+@property
+def color(self):
+    return self._color
+
+
